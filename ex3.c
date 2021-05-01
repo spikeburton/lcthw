58a8f3b381fe5c3157146cb75315b1c64e11f5f8
vagrant <vagrant@ubuntu>
2021-05-01 17:34:25 +0000

    fix ex3

--- ex3.c
+++ ex3.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    int age = 100;
+    int age = 10;
     int height = 72;
 
-    printf("I am %d years old.\n", argv);
+    printf("I am %d years old.\n", age);
     printf("I am %d inches tall.\n", height);
 
     return 0;
